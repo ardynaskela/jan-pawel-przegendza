a09d2acb3ec1589835ffa87e221307b995bea382
ardynaskela <serwisyap@przegendza.pl>
2026-01-02 00:07:26 +0000

    modified:   src/components/navigation.tsx

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -38,21 +38,16 @@ export default function Navigation({ children }: { children: React.ReactNode; })
                                 <MobileLink href="/media"> Media </MobileLink>
                                 <MobileLink href="/contact"> Contact </MobileLink>
 
-                                <motion.button
-                                    type="button"
-                                    onClick={() => setIsOpen(false)}
-                                    initial={{ transform: "translateY(100px)", filter: "blur(20px)", opacity: 0 }}
-                                    transition={{ type: "spring", stiffness: 25, damping: 2, mass: 0.1 }}
-                                    animate={{ transform: "translateY(0px)", filter: "blur(0px)", opacity: 1 }}
-                                    exit={{ transform: "translateY(100px)", filter: "blur(20px)", opacity: 0 }}
-                                    className="w-fit flex items-center justify-center gap-3 text-lg sm:text-2xl text-zinc-100 cursor-pointer">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" aria-hidden>
-                                        <title>Close Icon</title>
-                                        <path fill="currentColor" d="M22 12c0 5.523-4.477 10-10 10S2 17.523 2 12S6.477 2 12 2s10 4.477 10 10" opacity=".5" />
-                                        <path fill="currentColor" d="M8.97 8.97a.75.75 0 0 1 1.06 0L12 10.94l1.97-1.97a.75.75 0 1 1 1.06 1.06L13.06 12l1.97 1.97a.75.75 0 0 1-1.06 1.06L12 13.06l-1.97 1.97a.75.75 0 0 1-1.06-1.06L10.94 12l-1.97-1.97a.75.75 0 0 1 0-1.06" />
-                                    </svg>
-                                    Close
-                                </motion.button>
+                             <motion.button
+                             type="button"
+                             onClick={() => setIsOpen((prev) => !prev)}
+                            className={clsx(
+                                 "w-fit text-base sm:text-2xl px-5 py-2 rounded-full transition-all duration-300 ease-in-out whitespace-nowrap",
+                                  "text-zinc-900 bg-zinc-100 hover:outline-2 outline-offset-2 outline-zinc-100",
+                                   )}
+                                   >
+                                    {isOpen ? "Close" : "Menu"}
+                                    </motion.button>
                             </div>
                         </motion.dialog>
                     </>
